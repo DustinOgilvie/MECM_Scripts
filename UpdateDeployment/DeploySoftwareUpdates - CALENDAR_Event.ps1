6afc6f3b95e72284fc691f03fdb4b368a2865826
DustinOgilvie <53351817+DustinOgilvie@users.noreply.github.com>
2020-08-05 12:48:43 -0500

    Add files via upload

---
+++ UpdateDeployment/DeploySoftwareUpdates - CALENDAR_Event.ps1
@@ -0,0 +1,38 @@
+﻿# To be used with DeploySoftwareUpdates.ps1 script. By using the same UpdateControl csv file, you can create calendar
+# events to send to the stakeholders, notifying them of when the deployment will occur.
+
+
+# Get the details for the deployment from the text file. (EDIT THIS)
+$DeploymentDetail = Get-Content "C:\Location\UpdateControl.txt"
+
+# Process each line of the input file.
+ForEach ($Detail in $DeploymentDetail)
+{
+    # Each item in the input file is separated by a comma.  Split those values into their corresponding intended variable
+    # and use as options for Start-CMSoftwareUpdateDeployment.
+    $split=$Detail.split(",")
+    $UpdateGroupName = $Split[0]
+    $DeploymentTitle = $Split[1]
+    $TargetCollection = $Split[2]
+    $DeploymentDateTime = $Split[3]
+    $DeadlineDateTime = $Split[4]
+
+
+ #Create Calendar Event for Deployment.
+    $ol = New-Object -ComObject Outlook.Application
+    $meeting = $ol.CreateItem('olAppointmentItem')
+    $meeting.Subject = $DeploymentTitle
+    $meeting.Body = $DeploymentTitle
+    $meeting.Location = 'Virtual'
+    $meeting.ReminderSet = $true
+    $meeting.Categories = 'Patching'
+    #$meeting.Importance = 1
+    $meeting.MeetingStatus = [Microsoft.Office.Interop.Outlook.OlMeetingStatus]::olMeeting
+    $meeting.Recipients.Add('GroupEmail@email.com')
+    $meeting.ReminderMinutesBeforeStart = 300
+    $meeting.Start = [datetime]$DeadlineDateTime
+    #$meeting.End = [datetime]"10/11/2019 19:00"
+    $meeting.Duration = 360
+    #$meeting.File = $CollectionMembersFile
+    $meeting.Send()
+}
---
+++ UpdateDeployment/DeploySoftwareUpdates - NEW.ps1
@@ -0,0 +1,44 @@
+﻿# This script will read a file as input.  Each line of that file should contain the Software Update Group, a Deployment 
+# Name and the target collection for the deployment.  These values should be seperated by a comma.  
+# Example:  Update Group 1,Test Deployment 1,All Systems
+# 
+# The script will then build all deployments against the deployments groups configured in the file using the parameters
+# for the deployment as detailed on the Start-CMSoftwareUpdateDeployment cmdlet.  
+# NOTE 1:  It would be easy to allow user define settings for each Start-CMSoftwareUpdateDeployment parameter if desired.
+# NOTE 2:  If an update group contains updates that are superseded or expired then deployments may fail to be created.
+# Ensure that the update group being targeted for deployment is properly maintained either manually or using a seperate
+# script.
+# NOTE 3:  The script uses a text file as an input source.  The script could be modified to leverage whatever input source
+# is preferred, such as an Excel spreadsheet, SharePoint list, web page, etc.
+
+Param(
+    [Parameter(Mandatory = $true)]
+    $SiteServerName,
+    [Parameter(Mandatory = $true)]
+    $SiteCode
+    )
+
+Import-module ($Env:SMS_ADMIN_UI_PATH.Substring(0,$Env:SMS_ADMIN_UI_PATH.Length-5) + '\ConfigurationManager.psd1')
+
+# Connect to discovered top level site
+cd $SiteCode":"
+
+# Get the details for the deployment from the text file. (EDIT THIS)
+$DeploymentDetail = Get-Content "C:\Location\UpdateControl.txt"
+
+# Process each line of the input file.
+ForEach ($Detail in $DeploymentDetail)
+{
+    # Each item in the input file is separated by a comma.  Split those values into their corresponding intended variable
+    # and use as options for Start-CMSoftwareUpdateDeployment.
+    $split=$Detail.split(",")
+    $UpdateGroupName = $Split[0]
+    $DeploymentTitle = $Split[1]
+    $TargetCollection = $Split[2]
+    $DeploymentDateTime = $Split[3]
+    $DeadlineDateTime = $Split[4]
+    # The Start-CMSoftwareUpdateDeployment cmdlet creates the deployment based on a combination of static defaults configured
+    # here and variables passed in from the text file.
+    New-CMSoftwareUpdateDeployment -SoftwareUpdateGroupName $UpdateGroupName -CollectionName $TargetCollection -DeploymentName $DeploymentTitle -Description "Deployment Created by Powershell" -DeploymentType Required -VerbosityLevel AllMessages -TimeBasedOn LocalTime -AvailableDateTime $DeploymentDateTime -DeadlineDateTime $DeadlineDateTime -UserNotification DisplayAll -DownloadFromMicrosoftUpdate $True -UseMeteredNetwork $False -AcceptEula
+
+}
